f85ce7f81ed71ceccb8335889eff489938061e28
ibrahim nagib <ibrahimn@cs.utexas.edu>
2014-10-15 19:07:51 -0500

    adding

---
+++ voting.py
@@ -0,0 +1,19 @@
+
+
+class Ballot:
+	def __init__(self):
+		pass
+
+class Candidate:
+	def __init__(self, name, num_votes):
+		self.name = name
+		self.num_votes = 0
+	def __next__:
+		pass
+
+
+line = infile.readline()
+num_cand = infile.readline()
+while line != "":
+	line = infile.readline()
+	
