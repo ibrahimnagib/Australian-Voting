cc76f0cadd5872a587a76339f5904278d42e4c27
ibrahimnagib <ibrahim.nagib@utexas.edu>
2014-10-22 20:52:02 -0500

    Update voting.py

--- voting.py
+++ voting.py
@@ -28,48 +28,42 @@ class Candidate:
 
 	def mod_ballot(self):
 		for x in self.cand_ballot:
-			del x.votes[0]
-	#wont we have to reassign the ballot to someone after modifying it ????		
+			del x[0]
+
+	def get_vote(self):
+		for x in self.cand_ballot:
+			return x[0]		
 
 class Ballot: 
 	def __init__(self, prefs):
 		self.index = 0
 		self.votes = list(prefs)
 
-	# def get_ballot(self):
-	# 	return self.votes
-
-
 def read(r):
 	infile = open(r,'r')
 	num_elections = int(infile.readline())
+	assert type(num_elections) == int
 	infile.readline()
+
 	while num_elections != 0:
 		num_candidates = int(infile.readline())
+		assert type(num_candidates) == int
 		cand_list = []
-		# print (num_candidates)
 		for i in range (num_candidates):
 			cand_list.append(Candidate(infile.readline().rstrip(), i+1 ))
 
 		line = infile.readline()
-		while line != '':
+		while line != '' and line != '\n':
 			tmp = Ballot(line.rstrip().split(' '))
-			#print(tmp[0])
 			ballot_list.append(tmp.votes)
 			cand_list[int(tmp.votes[0])-1].cache_ballot(tmp.votes)
 			line = infile.readline()
-		# print (cand_list[1].view_ballot())
-		# print (ballot_list)
 		total_ballot = len(ballot_list)	
 		winners = solve_election(cand_list, total_ballot)
-
-
-		# for i in winners:
-		# 	print (i.name)
-		infile.readline()	
+		#w.write(winners)
 		num_elections -= 1
 
-	return num_elections, cand_list, ballot_list, num_candidates, total_ballot	
+	return num_elections, cand_list, ballot_list, num_candidates, total_ballot, winners
 
 def test_winner(vote_dict, total_ballot):
 	haswinner = False
@@ -96,34 +90,32 @@ def loser(vote_list, vote_dict, cand_list):
 		if v == loser_vote:
 			loser_list.append(k)
 
-	loser_ballot =[]
-	for n in cand_list:
-		for loser in loser_list:		
-			if n.name == loser:
-				loser_ballot.append(n.view_ballot())
-				cand_list.remove(n)	
+	cand_loser = []
+	tmp1 = cand_list
+	for loser in loser_list:
+		for n in tmp1:
+			if loser == n.name:
+				cand_loser.append(n)
+				cand_list.remove(n)
 
-	loser_ballot = loser_ballot[0]
-	for b in loser_ballot:
-		del b[0]
-		
-	# print (loser_ballot)
 	cand_id_list = []
 	for cand in cand_list:
-		cand_id_list.append(cand.number)
-	for b in loser_ballot:
-		while int(b[0]) not in cand_id_list:
-			del b[0]
-		for cand in cand_list:
-			if int(b[0]) == cand.number:
-				cand.cache_ballot(b) 
-				loser_ballot.remove(b)
-	# print ('test1')
+		cand_id_list.append(cand.number)	
+
+	for loser in cand_loser:
+		loser.mod_ballot()
+		ball = loser.view_ballot()
+		for b in ball:
+			while int(b[0]) not in cand_id_list:
+				b.remove(b[0])
+			for cand in cand_list:
+				if int(b[0]) == cand.number:
+					cand.cache_ballot(b)
+
 	return cand_list			
 	
-def solve_election (cand_list, total_ballot): # added total ballot to check for majority 
+def solve_election (cand_list, total_ballot, w):
 
-#creating a vote list of votes collected by each candidate
 	while True:
 		vote_dict = {}
 		vote_list = []
@@ -132,24 +124,21 @@ def solve_election (cand_list, total_ballot): # added total ballot to check for 
 			vote_list.append(n.cand_votecount())
 
 		Winner, hasWinner = test_winner(vote_dict, total_ballot)
-		# print hasWinner
-		if(hasWinner == True):
-			print ('winner')
-			# print (Winner)
-			return [Winner]
+
+		if(hasWinner):
+			return (Winner, '\n')
+			break
 
 		if(test_tie(vote_list)):
-			print ('tie')
-			return cand_list
-
-
+			x = ''
+			for cand in cand_list:
+				x += str(cand.name) + '\n'
+			return (x)
+			break
+		
 		cand_list = loser(vote_list, vote_dict, cand_list)
 		
+def final_election(r, w):
+	num_elections, cand_list, ballot_list, num_candidates, total_ballot, winners = read(r)
+	w.write(winners)
 
-
-
-def main():
-	num_elections, cand_list, ballot_list, num_candidates, total_ballot = read('read.txt')
-
-
-main()	
